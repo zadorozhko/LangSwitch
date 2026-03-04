9a2572e378f335652982a8102179f2bdecd2fa3e
Ilia Zadorozhko <zadorozhko@gmail.com>
2026-03-04 15:53:36 +0200

    Fix String extension

--- LangSwitch/KeyboardManager.swift
+++ LangSwitch/KeyboardManager.swift
@@ -8,17 +8,32 @@
 import AppKit
 import Carbon
 
+extension String {
+    var length: Int {
+        return count
+    }
+    subscript (i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, length) ..< length]
+    }
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+}
+
 extension StringProtocol {
     func distance(of element: Element) -> Int? {
         guard let idx = firstIndex(of: element) else { return nil }
         return self.distance(from: startIndex, to: idx)
-    }
-}
-
-extension String {
-    subscript(i: Int) -> String {
-        let idx = self.index(self.startIndex, offsetBy: i)
-        return String(self[idx])
     }
 }
 
